10643e957460305401ef087c3b0c0ff3d35fa747
yisus <guevarayisus1@gmail.com>
2022-05-28 10:04:25 -0500

    fixed(handler): deleted unnecesary comments

--- handler.js
+++ handler.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const {obtenerDatos} = require('./getData');
 
 app.use(bodyParser.urlencoded({extended: true}));
-const cpQro = require('./cpqro.json');
 
 app.get('/postales/:postalcode', (req, res) =>{
   const postalCode = req.params.postalcode;
@@ -14,23 +13,3 @@ app.get('/postales/:postalcode', (req, res) =>{
 });
 
 module.exports.generic = serverless(app);
-/*
-module.exports.municipios = async (event) => {
-  const cp = event.pathParameters.cp;
-  const data = require('./cpqro.json');
-  const colonias =[];
-  const resultados = data.filter(objecto => objecto.cp==cp);
-  colonias.push(resultados);
-  
-  if(colonias.length==0){
-    return {error: 'no hay colonias', colonias}
-  }
-  return {
-    colonias,
-    error: null
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-*/
